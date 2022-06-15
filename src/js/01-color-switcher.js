018cee2aa483afec75f52593673b4b67b61a85df
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-06-15 22:13:29 +0300

    1task-done!

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+stopBtn.setAttribute('disabled', 'true');
+
+startBtn.addEventListener('click', start);
+stopBtn.addEventListener('click', stop);
+
+function start() {
+  startBtn.setAttribute('disabled', 'true');
+  stopBtn.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stop() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'true');
+}

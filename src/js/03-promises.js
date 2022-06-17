cf41cc83cfc9113b1294b98474325d96db6a9e93
Vitaliy Zhoglo <zhoglo.vitaliy@gmail.com>
2022-06-17 17:01:50 +0300

    3task -done!

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const { delay, step, amount } = form.elements;
+
+form.addEventListener('submit', onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  let userDelay = Number(delay.value);
+  const userStep = Number(step.value);
+  const userAmount = Number(amount.value);
+
+  if (userDelay < 0 || userStep < 0 || userAmount < 0) {
+    return;
+  }
+  for (let i = 1; i <= userAmount; i++) {
+    createPromise(i, userDelay).then(onSuccess).catch(onError);
+    userDelay += userStep;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((fulfill, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        fulfill({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
